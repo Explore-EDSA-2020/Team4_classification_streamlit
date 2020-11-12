7f3a90fdb8f2d39c4d15caa6c6d03eca45e02ecd
Prescam3 <60913826+Prescam3@users.noreply.github.com>
2020-11-12 16:07:44 +0200

    lot of changes

--- base_app.py
+++ base_app.py
@@ -38,20 +38,13 @@ import plotly.figure_factory as ff
 import matplotlib.pyplot as plt
 import seaborn as sns
 from wordcloud import WordCloud
-#import spacy_streamlit
 from PIL import Image
 import spacy
 import string
 import re
 import os
 
-# import nltk
-# from nltk.corpus import stopwords
-# from nltk.tokenize import word_tokenize
 
-# porterStemmer = nltk.PorterStemmer()
-# wordNetLemma = nltk.WordNetLemmatizer()
-# stopword = stopwords.words('english')
 
 # Vectorizer
 news_vectorizer = open("resources/tfidfvect.pkl","rb")
@@ -59,7 +52,6 @@ tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl f
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-# nlp = spacy.load('en')
 
 # Load your raw data
 raw = pd.read_csv("resources/train.csv")
@@ -73,7 +65,6 @@ def main():
 
 	html_temp = """
 	<div style="background-color:{};padding:10px;border-radius:10px;margin:10px;">
-	<img src="resources/imgs/EDSA_LOGO.PNG" alt="edsa logo" width="500" height="600">
 	<h1 style="color:{};text-align:center;">EDSA 2020:Climate Change Belief Analysis</h1>
 	</div>
 	"""
@@ -82,40 +73,14 @@ def main():
 	<div style="background-color:#464e5f;padding:10px;border-radius:10px;margin:10px;">
 	<h1 style="color:white;text-align:center;">CLASSIFICATION PREDICT</h1>
 	<h2 style="color:white;text-align:center;">TEAM:4</h2>
-	<h3 style="color:white;text-align:right;">2020/11/03</h3>
+	<h3 style="color:white;text-align:right;">2020/11/18</h3>
 	</div>
 	"""
-	article_temp ="""
-	<div style="background-color:#464e5f;padding:10px;border-radius:5px;margin:10px;">
-	<h4 style="color:white;text-align:center;">{}</h1>
-	<h6>Author:{}</h6> 
-	<h6>Post Date: {}</h6>
-	<p style="text-align:justify">{}</p>
-	</div>
-	"""
-	head_message_temp ="""
-	<div style="background-color:#464e5f;padding:10px;border-radius:5px;margin:10px;">
-	<h4 style="color:white;text-align:center;">{}</h1>
-	<h6>Author:{}</h6> 
-	<h6>Post Date: {}</h6> 
-	</div>
-	"""
-	full_message_temp ="""
-	<div style="background-color:silver;overflow-x: auto; padding:10px;border-radius:5px;margin:10px;">
-	<p style="text-align:justify;color:black;padding:10px">{}</p>
-	</div>
-	"""
-
 	
-	# st.markdown(title_temp, unsafe_allow_html=True)
-	# st.markdown(article_temp, unsafe_allow_html=True)
-	# st.markdown(head_message_temp, unsafe_allow_html=True)
-	# st.markdown(full_message_temp, unsafe_allow_html=True)
 
 	# Creates a main title and subheader on your page -
 	# these are static across all pages
-	# st.title("Tweet Classifer")
-	# st.subheader("Climate change tweet classification")
+
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
@@ -170,28 +135,6 @@ Variable definitions:
 				st.write(raw[['sentiment', 'message']]) # will write the df to the page
 
 
-# Building out the "Text Classification" page
-	if selection == "Text Classification":
-		# markup(selection)
-		
-		our_image = Image.open(os.path.join('resources/imgs/logo.png'))
-		st.image(our_image)
-
-		menu = ["Home","NER"]
-		choice = st.selectbox("Menu",menu)
-
-		# if choice == "Home":
-		# 	st.subheader("Tokenization")
-		# 	raw_text = st.text_area("Your Text","Enter Text Here")
-		# 	docx = nlp(raw_text)
-		# 	if st.button("Tokenize"):
-		# 		spacy_streamlit.visualize_tokens(docx,attrs=['text','pos_','dep_','ent_type_'])
-
-		# elif choice == "NER":
-		# 	st.subheader("Named Entity Recognition")
-		# 	raw_text = st.text_area("Your Text","Enter Text Here")
-		# 	docx = nlp(raw_text)
-		# 	spacy_streamlit.visualize_ner(docx,labels=nlp.get_pipe('ner').labels)
 
 	# Building out the predication page
 	if selection == "Predictions":
@@ -339,13 +282,6 @@ Variable definitions:
 		markup(selection)
 		train_df = raw
 
-		#Show the counts
-
-		#plot and visualize the counts
-		if st.checkbox("Models and their Accuracy score"):
-			st.subheader("Models based on raw data and the accuracy score")
-			st.image('resources/accuracy.png', channels="BGR")
-
 
 		#plot and visualize the most used words in wordcloud
 		if st.checkbox("word cloud"):
@@ -373,35 +309,12 @@ Variable definitions:
 		if st.checkbox("handles of the most tweets"):
 			st.subheader("handles of the tweets per message(news)")
 			st.image('resources/hand_news.png', channels="BGR")
-
-			st.subheader("handles of the tweets per message(Pro)")
-			st.image('resources/hand_pro.png', channels="BGR")
-
-
-			st.subheader("handles of the tweets per message(Anti)")
-			st.image('resources/handl_anti.png', channels="BGR")
-
-
-			st.subheader("handles of the tweets per message(Neutral)")
-			st.image('resources/hand_neutr.png', channels="BGR")
 			
-
 
 		if st.checkbox("frequent words used most in tweets"):
 			st.subheader("frequent words used most in tweets(news)")
 			st.image('resources/words_news.png', channels="BGR")
 
-
-			st.subheader("frequent words used most in tweets(Pro)")
-			st.image('resources/Pro_news.png', channels="BGR")
-
-
-			st.subheader("frequent words used most in tweets(Anti)")
-			st.image('resources/anti.png', channels="BGR")
-
-
-			st.subheader("frequent words used most in tweets(Neutral)")
-			st.image('resources/neutral.png', channels="BGR")
 
 
 		if st.checkbox("frequent # used most in tweets"):
@@ -409,16 +322,6 @@ Variable definitions:
 			st.image('resources/#_news.png', channels="BGR")
 
 
-			st.subheader("frequent # used most in tweets(Pro)")
-			st.image('resources/Pro_#.png', channels="BGR")
-
-
-			st.subheader("frequent # used most in tweets(Anti)")
-			st.image('resources/anti_#.png', channels="BGR")
-
-
-			st.subheader("frequent # used most in tweets(Neutral)")
-			st.image('resources/neutral_#.png', channels="BGR")	
 
 		if st.checkbox("metrics"):
 			st.subheader('plot the PCC figure')
@@ -458,47 +361,6 @@ def markup(heading):
 	html_temp = """<div style=background-color:{};padding:10px;boarder-radius:10px"><h1 style="color:{};text-align:center;">"""+heading+"""</h1>"""
 	st.markdown(html_temp.format('royalblue','white'), unsafe_allow_html=True)
 
-# Data Cleaning
-# def clean_message(message):
-#     str(message).lower()
-#     regrex_pattern = re.compile(pattern = "["
-#       u"\U0001F600-\U0001F64F"  # emoticons
-#         u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-#         u"\U0001F680-\U0001F6FF"  # transport & map symbols
-#         u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-#         u"\U00002500-\U00002BEF"  # chinese char
-#         u"\U00002702-\U000027B0"
-#         u"\U00002702-\U000027B0"
-#         u"\U000024C2-\U0001F251"
-#         u"\U0001f926-\U0001f937"
-#         u"\U00010000-\U0010ffff"
-#         u"\u2640-\u2642" 
-#         u"\u2600-\u2B55"
-#         u"\u200d"
-#         u"\u23cf"
-#         u"\u23e9"
-#         u"\u231a"
-#         u"\ufe0f"  # dingbats
-#         u"\u3030"
-#          "]+", flags = re.UNICODE)
-#     message = regrex_pattern.sub(r'',message) # remove emojis
-#     # Remove user @ references and '#' from tweet
-#     message = re.sub(r'@[A-Za-z0-9]+','',message) ##Remove @aderate
-#     message = re.sub(r'RT[\s]+', '', message) ## remove RT Retweets
-#     message = re.sub(r'https?:\/\/\S+', '', message) ##remove hyperlink
-#     message =  ''.join([char for char in message if char not in string.punctuation]) ## remove puntuations i.e. ('!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~')
-#     message = re.sub(r'((www\.[^\s]+)|(https?://[^\s]+))', '', message) # remove URLs
-#     message = re.sub(r'@[^\s]+', '', message) # remove usernames
-#     message = re.sub(r'#[A-Za-z0-9]+', '', message) #get rid of hashtags
-#     message = message.translate(str.maketrans('', '', string.punctuation))
-#     message_tokens = word_tokenize(message)
-#     filtered_message = [word.lower() for word in message_tokens if word not in stopword]
-
-#     stemmed_words = [porterStemmer.stem(word) for word in filtered_message]
-#     lemma_words = [wordNetLemma.lemmatize(word) for word in stemmed_words]
-
-#     return ' '.join(lemma_words)
-# train_df['message'] = train_df['message'].apply(clean_message)
 
 #Function to lable our Sentiments
 def getAnalysis(score):
@@ -513,11 +375,6 @@ def getAnalysis(score):
 train_df['Analysis'] = train_df['sentiment'].apply(getAnalysis)
 train_df['msg_len'] = train_df['message'].apply(lambda x: len(x))
 
-# def punctuation_count(txt):
-# 	count = sum([1 for c in txt if c in string.punctuation])
-# 	return 100*count/(len(txt))	
-
-# train_df['punctuation_%'] = train_df['message'].apply(lambda x: punctuation_count(x))
 
 # Required to let Streamlit instantiate our web app.
 if __name__ == '__main__':
